44c1b56bf69beb2a910c5f10336a376c491d6b3d
Clueed <clueed@proton.me>
2023-03-27 16:25:47 +0200

    style: light mode message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -8,23 +8,18 @@ export default function Message ({ role, content, loading }) {
   return (
     <div
       className={
-        'flex w-full gap-3 px-5 py-5 align-middle rounded-lg text-stone-50 bg-gray-50 dark:bg-gray-700' +
+        'flex w-full gap-3 px-5 py-5 align-middle rounded-lg text-stone-900 bg-gray-50 dark:bg-gray-700' +
         (role === 'user' ? ' opacity-80' : '')
       }
     >
       <div>
-        {role === 'system' && (
-          <SparklesIcon className='w-6 h-6 dark:text-gray-500' />
-        )}
+        {role === 'system' && <SparklesIcon className='w-6 h-6' />}
         {role === 'assistant' && (
           <RocketLaunchIcon
-            className={
-              'dark:text-gray-500 w-6 h-6' +
-              (loading === true ? ' animate-spin' : '')
-            }
+            className={'w-6 h-6' + (loading === true ? ' animate-spin' : '')}
           />
         )}
-        {role === 'user' && <UserIcon className='w-6 h-6 dark:text-gray-500' />}
+        {role === 'user' && <UserIcon className='w-6 h-6' />}
       </div>
       {content && (
         <div
