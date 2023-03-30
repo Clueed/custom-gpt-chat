253598fa5861ae43995bf6a6d6a1ae117d02e65f
Clueed <clueed@proton.me>
2023-03-30 10:14:40 +0200

    feat: defaulth system message no prompt

--- README.md
+++ README.md
@@ -3,4 +3,24 @@
 
 # Custom GPT Chat
 
-A reimplementation of chatGPT designed to provide users more control over their chatbot by offering features like custom system prompts and model selection. This is an exercise project aimed at learning React and Redux.
+A reimplementation of chatGPT designed to provide users more control over their chatbot experience. This is an exercise project aimed at learning React, Redux, and Next.js.
+
+## Features
+
+_improving upon the official chatGPT_
+
+- Access to/customization of the system prompt
+- On-the-fly selection of GPT models
+- Faster processing with chatGPT premium speed at a fraction of the cost
+  - 200,000 2-3 sentence prompts for just $20 (one month of chatGPT Plus)
+- Prompt cost estimation
+- Improved delete message UI.
+
+## How to use
+
+> Your API key is at no point send to any other location then the official OpenAI API. For security reasons it is **not even** stored permanently **on your own device** which is why it needs to be input on each reload.
+
+1. Get your OpenAI API key at https://platform.openai.com/account/api-keys
+2. Go to https://custom-gpt-chat.vercel.app
+3. Input your API key
+4. Use just like chatGPT
--- src/slices/conversationSlice.jsx
+++ src/slices/conversationSlice.jsx
@@ -1,75 +1,80 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import queryChatGPT from '../components/OpenAIAPI'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import queryChatGPT from "../components/OpenAIAPI";
 
 const initialState = {
-  messages: {},
-  loading: false
-}
+  messages: {
+    systemMessage: {
+      content: "You are a helpful assistant.",
+      role: "system",
+    },
+  },
+  loading: false,
+};
 
 export const getResponse = createAsyncThunk(
-  'conversation/getResponse',
+  "conversation/getResponse",
   async (payload, { getState }) => {
-    const state = getState()
+    const state = getState();
 
-    const apiKey = state.openAIConfig.apiKey
-    const model = state.openAIConfig.model
+    const apiKey = state.openAIConfig.apiKey;
+    const model = state.openAIConfig.model;
 
-    const messages = state.conversation.messages
-    const messagesUUIDs = Object.keys(messages)
-    const messagesList = messagesUUIDs.map((uuid) => messages[uuid])
+    const messages = state.conversation.messages;
+    const messagesUUIDs = Object.keys(messages);
+    const messagesList = messagesUUIDs.map((uuid) => messages[uuid]);
     const messagesContentList = messagesList.map(
       ({ content, role, ...rest }) => ({ content, role })
-    )
+    );
 
     const res = await queryChatGPT(apiKey, model, messagesContentList).then(
       (data) => {
-        return JSON.stringify(data)
+        return JSON.stringify(data);
       }
-    )
-    return res
+    );
+    return res;
   }
-)
+);
 
 export const conversationSlice = createSlice({
-  name: 'conversation',
+  name: "conversation",
   initialState,
   reducers: {
     add: (state, action) => {
-      state.messages[crypto.randomUUID()] = action.payload
+      state.messages[crypto.randomUUID()] = action.payload;
     },
     clearExcept: (state, action) => {
       for (const key in state.messages) {
         if (key !== action.payload.uuid) {
-          delete state.messages[key]
+          delete state.messages[key];
         }
       }
     },
     remove: (state, action) => {
-      delete state.messages[action.payload.uuid]
+      delete state.messages[action.payload.uuid];
     },
     edit: (state, action) => {
-      state.messages[action.payload.uuid] = action.payload.message
-    }
+      state.messages[action.payload.uuid] = action.payload.message;
+    },
   },
   extraReducers: (builder) => {
     builder
       .addCase(getResponse.fulfilled, (state, { payload }) => {
-        const parsedPayload = JSON.parse(payload)
+        const parsedPayload = JSON.parse(payload);
 
         state.messages[crypto.randomUUID()] =
-          parsedPayload.data.choices[0].message
+          parsedPayload.data.choices[0].message;
 
-        state.loading = false
+        state.loading = false;
       })
       .addCase(getResponse.rejected, (state) => {
-        state.loading = false
+        state.loading = false;
       })
       .addCase(getResponse.pending, (state) => {
-        state.loading = true
-      })
-  }
-})
+        state.loading = true;
+      });
+  },
+});
 
-export const { add, clearExcept, remove, edit } = conversationSlice.actions
+export const { add, clearExcept, remove, edit } = conversationSlice.actions;
 
-export default conversationSlice.reducer
+export default conversationSlice.reducer;
